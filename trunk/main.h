6e4cb80f7f1c1c0bd342e4a82735c98025b70249
matt.odille <matt.odille@029f095e-2af0-11df-b279-ed2be448802f>
2010-03-10 15:27:45 +0000

    added motorSynch code; changed the "60" constant to a DEFINE;  added placeholders in main for some TODO stuff;   updated main.h comments and func protos

--- trunk/main.h
+++ trunk/main.h
@@ -1,34 +1,27 @@
 
-
-
-
-typedef enum {
+/**********************************************************************************
+* Typedefs
+*********************************************************************************/
+typedef enum{
   west = 1,   // 0001
   north = 2,  // 0010
   east = 4,   // 0100
   south = 8   // 1000
 } walls;
 
-
-
 typedef enum{
     dWest = 0,
     dNorth = 90,
     dEast = 180,
     dSouth = 270
-
 }directions;
 
 typedef struct{
-
   int x;
   int y;
 }coord;
 
-
-
-
-typedef struct {
+typedef struct{
   walls cell_walls;
   bool visited;
 } cell;
@@ -36,22 +29,25 @@ typedef struct {
 typedef struct{
   int x;
   int y;
-
   int angle;
 } position;
 
 
-
+/**********************************************************************************
+* Global Variables
+*********************************************************************************/
 position curr_position;
 
 
+/**********************************************************************************
+* Function prototypes
+*********************************************************************************/
 void halt();
 void set_turret_angle(directions angle);
 void set_base_angle(directions angle);
 void turn_base(int angle, int direction);
 void turn_turret(int angle, int direction);
 void dash();
-void initialize_maze();
 int get_sonar();
 void scan_cell();
 void scan_wall(walls w);
@@ -59,6 +55,7 @@ void align_turret();
 bool get_neighbor_coordinate(int x, int y, walls dir, coord *n);
 float get_distance(int x1, int y1, int x2, int y2);
 walls choose_best_cell();
-void get_cell_pixel_origin(int x, int y, coord *n);
-void get_cell_pixel_center(int x, int y, coord *n);
+void display_map();
+void draw_cell(int x, int y);
 void draw_cell_wall(int x, int y, walls dir);
+void get_cell_pixel_origin(int x, int y, coord *n);
